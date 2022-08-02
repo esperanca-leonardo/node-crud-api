ec917dad40b8d56081e1edfd19416aaa0bc26fa1
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2022-08-01 23:13:00 -0300

    arquivo .env criado e configurado com sucesso

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 
 const User = require('./models/User')
 
@@ -21,8 +22,8 @@ app.get('/', (request, response) => {
 
 const port = 3000
 
-const DB_PASSWORD = '9runJNaWURWb8QU4'
-const DB_USER = 'antares'
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
 const connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@node-api.hzzdf32.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose
